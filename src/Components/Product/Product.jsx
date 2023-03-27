d1091211812503dba7d3a1841627c8f48c7de3cf
Subhana <subhana5721194@gmail.com>
2023-03-27 11:43:35 +0600

    added product

---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    // console.log(props.product);
+    const { img, name, seller, ratings, price } = props.product;
+    return (
+
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className="product-info">
+                <h6 className="product-name">{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer:{seller}</p>
+                <p>Rating: {ratings} Star</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
+        </div>
+
+    );
+};
+
+export default Product;
